e6f47bf75ae50751cd4f364dbf90c7549f9af606
PingJing2025 <1832945718nancy@gmail.com>
2025-02-17 15:31:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ import scipy
 import numpy as np
 
 A=np.array([[1,2],[3,4]])
+eigenvalues, eigenvectors = scipy.linalg.eig(A)
+
+print("eigenvalues:",eigenvalues)
+print("eigenvalues:\n",eigenvectors)
